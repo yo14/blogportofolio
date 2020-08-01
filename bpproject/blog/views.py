21670934f5b1c69999754627e22ab6de76bc1c43
yo14 <ifliandry11@gmail.com>
2020-08-01 12:25:04 +0700

    Setup static folder for image

--- bpproject/blog/views.py
+++ bpproject/blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Blog
 
 def all_blogs(request):
-    return render(request, 'blog/all_blogs.html')
+    blogs = Blog.objects.order_by('-date')[:5]
+    return render(request, 'blog/all_blogs.html', {'blogs':blogs})
